7f171095447b7517fca5c40f1623d3b4581c36af
Matthew Welch <dmw9400@gmail.com>
2017-11-22 19:10:48 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -58,3 +58,21 @@ def bulk_tweet_shortener(tweet_array)
   end
 
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if selective_tweet_shortener(tweet).length > 140
+    selective_tweet_shortener(tweet)[0..136] + "..."
+  else
+    selective_tweet_shortener(tweet)
+  end
+
+
+end
