a1f8bcf1b4ea53fe67da0d3c64549fbe0df356f4
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 21:17:37 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  replacing_words = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+
+  replacing_words
+end
+
+def word_substituter(tweet)
+  replacing_words = dictionary
+  words = tweet.split(" ")
+  words.each_with_index do |word, i|
+    replacing_words.each do |replacer|
+      #binding.pry
+      if word == replacer[0]
+        #binding.pry
+        word[i] = replacer[1]!
+      end
+    end
+  end
+  words.join(" ")
+end
+
+end
