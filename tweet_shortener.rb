e1fbbfe8ed8448c7e85fa4b89491f9e4e8e0a635
Julien Tregoat <jtregoatlearn@gmail.com>
2017-12-05 21:13:28 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,46 +27,26 @@ end
 
 def bulk_tweet_shortener(tweets)
   tweets.each do |tweet|
-    tweet = tweet.split(" ")
-    tweet.each_with_index do |word, index|
-      if dictionary.include?(word.downcase)
-        tweet[index] = dictionary[word.downcase]
-      end
-    end
-    puts tweet.join(" ")
+    puts word_substituter(tweet)
   end
 end
 
 def selective_tweet_shortener(tweet)
     if tweet.size > 140
-      tweet = tweet.split(" ")
-      tweet.each_with_index do |word, index|
-        if dictionary.include?(word.downcase)
-          tweet[index] = dictionary[word.downcase]
-        end
-      end
-      tweet.join(" ")
+      word_substituter(tweet)
     elsif tweet.size < 140
       tweet
     end
 end
 
 def shortened_tweet_truncator(tweet)
-  if tweet.size > 140
-    tweet = tweet.split(" ")
-    tweet.each_with_index do |word, index|
-      if dictionary.include?(word.downcase)
-        tweet[index] = dictionary[word.downcase]
-      end
-    end
-    if tweet.join(" ").size > 140
-      tweet = tweet.join(" ")
-      tweet[137..-1] = "..."
-      tweet
-    else
-      tweet.join(" ")
-    end
+  subbed = word_substituter(tweet)
+  if subbed.size > 140
+    subbed[137..-1] = "..."
+    subbed
   elsif tweet.size < 140
     tweet
+  else
+    subbed
   end
 end
